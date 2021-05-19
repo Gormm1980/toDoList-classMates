62eb06287db0c11d4f5b67a0e659241cf017d960
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-19 16:17:01 +0000

    add deleteTask

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -7,7 +7,10 @@ const ToDoList = () => {
 		const newTodos = todos.concat({ title: task, id: todos.length + 1 });
 		setTodos(newTodos);
 	};
-
+	const deleteTask = taskId => {
+		const removeTask = todos.filter(task => task.id != taskId);
+		setTodos(removeTask);
+	};
 	return (
 		<div>
 			<input
@@ -22,7 +25,14 @@ const ToDoList = () => {
 			<ul>
 				{todos.map(todo => {
 					console.log(todo);
-					return <li key={todo.id}>{todo.title}</li>;
+					return (
+						<li key={todo.id}>
+							{todo.title}
+							<button onClick={() => deleteTask(todo.id)}>
+								X
+							</button>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
